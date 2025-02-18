b5de6ccdf87a23a593c384e9464a824bb89c6993
Biraj Baral <baralbiraj74@gmail.com>
2025-02-18 12:18:16 +0545

    Fix sign-in link path and update middleware redirects for improved navigation

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,27 +1,27 @@
-import {  NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
-    const token = await getToken({ req: request });
-    const url = request.nextUrl;
-  
-    console.log("Token:", token);
-    console.log("URL Pathname:", url.pathname);
-  
-    if (token && (url.pathname.startsWith('/signup') || url.pathname.startsWith('/signin'))) {
-      console.log("Redirecting to /Profile");
-      return NextResponse.redirect(new URL("/Profile", request.url));
-    }
-  
-    if (!token && url.pathname.startsWith('/Profile')) {
-      console.log("Redirecting to /signin");
-      return NextResponse.redirect(new URL("/signin", request.url));
-    }
-  
-    return NextResponse.next();
+  const token = await getToken({ req: request, secret: process.env.SECRET });
+  const url = request.nextUrl;
+
+  console.log("Token:", token);
+  console.log("URL Pathname:", url.pathname);
+
+  if (token && (url.pathname.startsWith('/signup') || url.pathname.startsWith('/signin'))) {
+    console.log("Redirecting to /admin");
+    return NextResponse.redirect(new URL("/admin", request.url));
   }
 
-export const config={
-    matcher:["/signin","/signup","/profile"]
+  if (!token && url.pathname.startsWith('/profile')) {
+    console.log("Redirecting to /signin");
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/signin", "/signup", "/profile"]
+};
